22c1029ace86f461de67390c7ce03448078af353
Elmer L. Marquez <62399619+leo-marqz@users.noreply.github.com>
2025-07-24 13:29:36 -0600

    Update script-for-debian-or-ubuntu-server.bash

--- script-for-debian-or-ubuntu-server.bash
+++ script-for-debian-or-ubuntu-server.bash
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# Actualizar el sistema
 sudo apt update -y
-
-# Instalar Apache
 sudo apt install apache2 -y
-
 sudo apt install git -y
-
-# Habilitar Apache en segundo plano
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
-# Filtrar las líneas que contienen "inet" o "inet6" en la salida de ifconfig y guardarlas en un archivo
 ip address | grep inet > info_red.txt
 
-# Leer el contenido del archivo en una variable
 info_red=$(cat info_red.txt)
 
-# Generar el contenido HTML con la variable que contiene la información de red
 html_content="
 <!DOCTYPE html>
 <html>
@@ -33,8 +24,6 @@ html_content="
 </html>
 "
 
-# Guardar el contenido HTML en un archivo
 echo "$html_content" > /var/www/html/index.html
 
-# Reiniciar el servidor Apache para aplicar los cambios
 sudo systemctl restart apache2
